d6736e1a07f0d683e1ffe7b65afe5cba00eaf51f
Nikohl <war2nep@gmail.com>
2021-12-07 17:33:55 +0900

    MOD:Set credentials to env vars

--- twpy.py
+++ twpy.py
@@ -1,25 +1,35 @@
 import tweepy
+import os
 import csv
 import pandas as pd
 
-auth = tweepy.OAuthHandler("2j8k9RRpvzJjsv52KkF3bor23", "ElEWe0wxBkJY80cRoyeiAMl8dExtZFvcchT56u4Isid4pWUy5q")
-auth.set_access_token("999693022014013440-4jsVX9nHSLYPWozxkn75rOs4cZtTTPQ", "ZYwNOodgzWI1T8dlCCyZJA9r7eYy3f0kZY6106NlpPi2X")
+if __name__ == '__main__':
+    try:
+        auth = tweepy.OAuthHandler(os.environ["CONSUMER_TOKEN"],os.environ["CONSUMER_TOKEN_SECRET"])
+        auth.set_access_token(os.environ["ACCESS_TOKEN"],os.environ["ACCESS_TOKEN_SECRET"])
 
-api = tweepy.API(auth,wait_on_rate_limit=True)
+        api = tweepy.API(auth,wait_on_rate_limit=True)
 
-#####Dover
-# Open/Create a file to append data
-csvFile = open('ua.csv', 'a')
-#Use csv Writer
-csvWriter = csv.writer(csvFile)
+        #####Dover
+        # Open/Create a file to append data
+        csvFile = open('dover.csv', 'a')
+        #Use csv Writer
+        csvWriter = csv.writer(csvFile)
 
-for tweet in tweepy.Cursor(api.search_tweets,q="#MeToo",count=100,lang="en",tweet_mode='extended').items():
-    print (tweet.created_at, tweet.user.screen_name, tweet.full_text)
-    csvWriter.writerow([tweet.created_at, tweet.full_text.encode('utf-8')])
-    print('＝＝＝＝＝＝＝＝＝＝')
-    print('twid : ',tweet.id)
-    print('user : ',tweet.user.screen_name)
-    print('date : ', tweet.created_at)
-    print(tweet.full_text)
-    print('favo : ', tweet.favorite_count)
-    print('retw : ', tweet.retweet_count)
+        for tweet in tweepy.Cursor(api.search_tweets,q='#ドーバー海峡 OR ＃ドーバー海峡',count=1500,tweet_mode='extended', result_type="mixed",include_entities=True).items():
+            if(tweet.user.screen_name == "_Nikohl"):
+                print(tweet.created_at, tweet.user.screen_name, tweet.full_text)
+                csvWriter.writerow([tweet.created_at, tweet.full_text])
+
+                #####
+                print('＝＝＝＝＝＝＝＝＝＝')
+                print('twid : ',tweet.id)
+                print('user : ',tweet.user.screen_name)
+                print('date : ', tweet.created_at)
+                print(tweet.full_text)
+                print('favo : ', tweet.favorite_count)
+                print('retw : ', tweet.retweet_count)
+                print('lang : ', tweet.lang)
+
+    except tweepy.TweepError as e:
+        print(e.reason)
